58d2df7f060e95fdedcbc6a4ee1f9ed51aa88be6
Seba Kloster <sebikloster@yahoo.com.ar>
2020-11-11 10:55:11 -0300

    Creando la conexion con MongoDB atlas

--- components/message/store.js
+++ components/message/store.js
@@ -1,11 +1,26 @@
-const list = [];
+const db = require("mongoose");
+const Model = require("./model");
+
+const uri =
+  "mongodb+srv://db_user_chat:ndgTqigpkG1AlUeD@cluster0.3vhjh.mongodb.net/db_chat?retryWrites=true&w=majority";
+
+db.Promise = global.Promise; //Le decimos a mongoose que si quiere usar promesas, que use las nativas de JS
+db.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  dbName: "db_chat",
+})
+  .then(() => console.log("[db] conectada con exito"))
+  .catch(() => console.error("[db]", err));
 
 function addMessage(message) {
-  list.push(message);
+  const myMessage = new Model(message);
+  myMessage.save();
 }
 
-function getMessages() {
-  return list;
+async function getMessages() {
+  const messages = await Model.find();
+  return messages;
 }
 
 module.exports = {
